20f5e857f99b6f50daad315ff2b95bbf62514185
Shaheen Toefy <shaheentoefy@gmail.com>
2026-09-09 14:44:35 +0200

    test: simplify Vitest configuration

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig } from "vitest/config";
 import { fileURLToPath, URL } from "node:url";
-import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL(".", import.meta.url)),
